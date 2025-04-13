83db77c2f6b323bab92b540fe9914a17cbeaa4f6
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-04-13 14:23:07 +0300

    Added optimistic update to enhance user experience

--- actions/posts.js
+++ actions/posts.js
@@ -1,8 +1,9 @@
 "use server";
 import { redirect } from "next/navigation";
 
-import { storePost } from "@/lib/posts";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 export async function createPost(prevState, formData) {
   const title = formData.get("title");
   const image = formData.get("image");
@@ -39,4 +40,9 @@ export async function createPost(prevState, formData) {
   });
 
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
 }
