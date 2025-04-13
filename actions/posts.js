4068710b56f3615ad2c6d18aa3e84b0c82d443f5
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-04-13 11:23:00 +0300

    Added Cloudinary as image upload service

---
+++ actions/posts.js
@@ -0,0 +1,42 @@
+"use server";
+import { redirect } from "next/navigation";
+
+import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/cloudinary";
+export async function createPost(prevState, formData) {
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required.");
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content is required.");
+  }
+
+  if (!image || image.size === 0) {
+    errors.push("Image is required.");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+  let imageURL;
+  try {
+    imageURL = await uploadImage(image);
+  } catch (error) {
+    throw new Error("Image upload failed.Please try again later.");
+  }
+  await storePost({
+    imageUrl: imageURL,
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect("/feed");
+}
